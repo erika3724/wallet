8d39c178a6d2a6b9842ffddad5970ac28e461228
Andre <andre@pop-os.localdomain>
2023-10-05 19:48:18 -0300

    req 5

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,48 @@
+import { useSelector } from 'react-redux';
+import { Gs } from './Header';
+
 function Table() {
-  return <div>Table</div>;
+  const { user, wallet } = useSelector((state: Gs) => state);
+
+  return (
+    <table style={ { textAlign: 'center' } }>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </thead>
+      <tbody>
+        {wallet.expenses.map((a, i) => (
+          <tr key={ a.id }>
+            <td>{ a.description }</td>
+            <td>{ a.tag }</td>
+            <td>{ a.method }</td>
+            <td>{ Number(a.value).toFixed(2) }</td>
+            <td>{ a.exchangeRates[a.currency].name }</td>
+            <td>
+              {Number(a.exchangeRates[a.currency]
+                .ask).toFixed(2)}
+
+            </td>
+            <td>
+              {(Number(a.exchangeRates[a.currency]
+                .ask) * Number(a.value)).toFixed(2) }
+            </td>
+            <td>Real</td>
+            <td />
+          </tr>
+        )) }
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
